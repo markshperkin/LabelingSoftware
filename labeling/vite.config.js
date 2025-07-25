1e67a61f52821504caa82c62b708a9b73e3b9673
mark shperkin <markshperkin1@gmail.com>
2025-07-25 12:32:25 -0400

    final commit

--- labeling/vite.config.js
+++ labeling/vite.config.js
@@ -1,4 +1,3 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/runs': 'http://localhost:5000',
-      '/runs/': 'http://localhost:5000', // for nested routes
-      '/data': 'http://localhost:5000'    // if you serve videos under /data
+      '/runs/': 'http://localhost:5000',
+      '/data': 'http://localhost:5000'
     }
   }
 })
